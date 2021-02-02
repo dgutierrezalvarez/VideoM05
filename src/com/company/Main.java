95953d8dfc82b5a8f124ad15753e251e1f25756a
dgutierrezalvarez <dgutierrez@elpuig.xeill.net>
2021-02-02 20:18:02 +0100

    tercer commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Adios mundo");
-        System.out.println("Hola rama");
+        System.out.println("hola rama");
     }
 }
